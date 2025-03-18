81caa7e15d5eedc3a753901030b4d980ae9d989e
Gustavo Yuri Nishi <gustavoyurinishi@gmail.com>
2025-03-18 17:03:45 -0300

    update

---
+++ aula05/script.js
@@ -0,0 +1,45 @@
+// window.alert("hello world");
+//console.log("hello world");
+
+//let nome = prompt("qual seu nome");
+//console.log(nome);
+
+//let i = 0;
+//while(i<10){
+//   console.log(i);
+    //i++;
+    // i = i + 1;
+//    i += 1;
+//}
+
+//console.log("for");
+//for(let i =0; i < 10; i++){
+//    console.log(i);
+//}
+
+//console.log("exercicio 1")
+//for(let i = 1; i < 100; i +=2){
+//    console.log(i)
+//}
+
+//console.log("exercicio 2")
+//for(let i = 0; i <= 500; i +=5){
+//    console.log(i)
+//}
+
+//let i = prompt("digite um numero:")
+//if(i < 0){
+//    console.log("não")
+//}
+//while(i > 0){
+//    console.log(i);
+//    i = i - 1
+//}
+
+let i = prompt("digite um numero")
+while(i > 1){
+    x = i * (i - 1);
+    i = i - 1
+    console.log(x)
+}
+
