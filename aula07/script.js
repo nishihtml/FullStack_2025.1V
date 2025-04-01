e9ecd3d8c34c081ef4de0002d0eed7e00a7cf767
Gustavo Yuri Nishi <gustavoyurinishi@gmail.com>
2025-04-01 15:46:14 -0300

    :3

--- aula07/script.js
+++ aula07/script.js
@@ -2,19 +2,40 @@ let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d')
 ctx.beginPath();
 ctx.lineWidth = 2;
-ctx.fillStyle = 'blue';
-ctx.strokeStyle = 'red';
-ctx.fillRect(0,0,50,50);
-ctx.strokeRect(10,10,50,50);
+ctx.fillStyle = 'black';
+ctx.strokeStyle = 'black';
+ctx.font = "90px Arial"
+ctx.textAlign = "center";
+ctx.fillText("brat",200,200);
+ctx.strokeText("brat",200,200)
 ctx.closePath();
 
-ctx.beginPath();
-ctx.lineWidth = 2;
-ctx.fillStyle = 'blue';
-ctx.strokeStyle = 'red';
-ctx.moveTo(200,150);
-ctx.lineTo(60,10);
-ctx.lineTo(60,250);
-ctx.fill();
-ctx.stroke();
-ctx.closePath();
+let canvas1 = document.getElementById('canvas1');
+let ctx1 = canvas1.getContext('2d')
+ctx1.beginPath();
+ctx1.lineWidth = 2;
+ctx1.strokeStyle = 'green';
+ctx1.arc(200,200,50,0, 1*Math.PI);
+ctx1.stroke();
+ctx1.closePath();
+ctx1.beginPath();
+ctx1.lineWidth = 2;
+ctx1.strokeStyle = 'green';
+ctx1.moveTo(0,200);
+ctx1.lineTo(400,200);
+ctx1.stroke()
+ctx1.closePath();
+ctx1.beginPath();
+ctx1.lineWidth = 2;
+ctx1.fillStyle = 'red';
+ctx1.strokeStyle = 'red';
+ctx1.fillRect(0,0,50,50);
+ctx1.strokeRect(0,0,50,50);
+ctx1.closePath();
+ctx1.beginPath();
+ctx1.lineWidth = 2;
+ctx1.fillStyle = 'blue';
+ctx1.strokeStyle = 'blue';
+ctx1.fillRect(400,0,50,50);
+ctx1.strokeRect(400,400,350,350);
+ctx1.closePath();
